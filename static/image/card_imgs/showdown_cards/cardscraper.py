528c8709bd67488b1e29347658179ebb20a322ad
Kyle Scott <kylehscott@gmail.com>
2018-02-09 23:46:30 -0700

    Deleted unused files

--- static/image/card_imgs/showdown_cards/cardscraper.py
+++
@@ -1,8 +0,0 @@
-import urllib
-
-card_no = 1
-
-for i in range(1230,1405):
-  urllib.urlretrieve("http://www.showdowncards.com/images/product/" + str(i) + ".jpg", str(card_no) + ".jpg")
-  print(str(card_no) + ".jpg downloaded.")
-  card_no += 1
